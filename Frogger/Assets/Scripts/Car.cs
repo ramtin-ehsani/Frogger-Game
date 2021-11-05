57259ec7a23cdf7aae4309aed8ce369b7048381f
Ramtin Ehsani <ramtin79e@gmail.com>
2021-11-05 19:58:25 +0330

    Add: basic car movement

---
+++ Frogger/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 1f;
+    void FixedUpdate()
+    {
+        var right = transform.up;
+        Vector2 forward = new Vector2(right.x, right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+    }
+}
